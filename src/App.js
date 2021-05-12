601617903db10000f892f95560ad98e71687c45b
Oliverac28 <55126545+Oliverac28@users.noreply.github.com>
2021-05-12 14:20:39 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { FaInstagram } from "react-icons/fa";
+import logo from "./assets/logo.svg";
+import sample from "./assets/sample.mp4";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <img src={logo} class="logo" alt="logo" />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <video className="videoTag" autoPlay loop muted>
+          <source src={sample} type="video/mp4" />
+        </video>
+        <h3 id="cs">Coming Soon...</h3>
       </header>
+
+      <div className="flex-container">
+        <a className="flex-item" id="mail" href="mailto:hello@zislocal.com">
+          hello@zislocal.com
+        </a>
+
+        <a className="flex-item" href="https://www.instagram.com/zis.local/">
+          <FaInstagram size="45px" />
+        </a>
+      </div>
     </div>
   );
 }
