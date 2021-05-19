dcf9d4aab1141d0e9d457e47fb3fbdf30d48a8a4
Oliverac28 <55126545+Oliverac28@users.noreply.github.com>
2021-05-19 06:05:17 +0400

    Updated video Added Facebook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { FaInstagram } from "react-icons/fa";
+import { FaFacebook, FaInstagram } from "react-icons/fa";
 import logo from "./assets/logo.svg";
 import sample from "./assets/sample.mp4";
 import "./App.css";
@@ -6,22 +6,28 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
+      <video className="videoTag" autoPlay loop muted>
+        <source src={sample} type="video/mp4" />
+      </video>
+
       <img src={logo} class="logo" alt="logo" />
+
       <header className="App-header">
-        <video className="videoTag" autoPlay loop muted>
-          <source src={sample} type="video/mp4" />
-        </video>
-        <h3 id="cs">Coming Soon...</h3>
+        <p id="cs">Coming Soon...</p>
       </header>
 
       <div className="flex-container">
         <a className="flex-item" id="mail" href="mailto:hello@zislocal.com">
-          hello@zislocal.com
+          <h4>hello@zislocal.com</h4>
         </a>
-
-        <a className="flex-item" href="https://www.instagram.com/zis.local/">
-          <FaInstagram size="45px" />
-        </a>
+        <div className="flex-item">
+          <a id="social" href="https://www.instagram.com/zis.local/">
+            <FaInstagram size="45px" />
+          </a>
+          <a id="social" href="https://www.facebook.com/zis.local/">
+            <FaFacebook size="43px" />
+          </a>
+        </div>
       </div>
     </div>
   );
